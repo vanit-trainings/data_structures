fa4cbd3748b794b569eba31b05ce2c62b2645f44
Narek <narekayd091@gmail.com>
2019-05-13 13:17:16 +0400

    Sorting already working!!!

--- Aydinyan/quickSortForList.js
+++ Aydinyan/quickSortForList.js
@@ -1,271 +1,252 @@
 const readline = require('readline-sync');
 
-let node = function(val, pr = null, ne = null) {
-	this.prev = pr;
-	this.next = ne;
-	this.value = val;
-}
-
-let list = function() {
-	this.length = 0;
-	this.first = null;
-	this.last = null;
-}
-
-list.prototype.push_back = function(data) {
-    if (this.length === 0) {
-        this.last = new node(data, null, null);
-        this.first = this.last;
-    }
-    else {
-        this.last.next = new node(data, this.last, null);
-        this.last = this.last.next;
-    }
-	this.length++;
-}
-
-list.prototype.push_front = function(data) {
-    this.first = new node(data, null, this.first);
-	if (this.length === 0) {
-        this.last = this.first;
-	}
-	this.length++;
-}
-
-list.prototype.insert = function (val, ind) {
-    if (ind === this.length) {
-        this.push_back(val);
-    }
-    else {
-    	let n = this.find(ind);
-    	if (n !== undefined) {
-            if (ind === 0) {
-                this.push_front(val);
-            }
-            else {
-                let np = n.prev;
-                n.prev = new node(val, np, n);
-                np.next = n.prev;
-
-            }
-            this.length++;
-        }
+class node {
+    constructor (val, p = null, n = null) {
+        this.value = val;
+        this.prev = p;
+        this.next = n;
     }
 }
 
-list.prototype.isEmpty = function() {
-    return this.length === 0;
-}
-
-list.prototype.find = function (ind) {
-	if (isNaN(Number(ind)) || ind >= this.length || ind < 0) {
-		return;
-	}
-	let tmp = this.first;
-	for (let i = 0; i < ind; i++) {
-		tmp = tmp.next;
-	}
-	return tmp;
-}
-
-list.prototype.remove = function(ind) {
-    let n = this.find(ind);
-    if (n !== undefined && this.length > 0) {
-        if (ind !== 0 && ind !== this.length - 1) {
-            n.prev.next = n.next;
-            n.next.prev = n.prev;
-        }
-        if (ind === 0) {
-            this.pop_front();
-        }
-        if (ind === this.length -1) {
-            this.pop_back();
-        }
+class list {
+    constructor () {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
     }
-}
 
-list.prototype.pop_front = function() {
-	if (this.length !== 0) {
-        let val = this.first.value;
-        this.first = this.first.next;
-        if (this.length !== 1) {
-            this.first.prev = null;
-        }
-        else {
-            this.last = this.first;
-        }
-        this.length--;
-		return val;
-    }
-    return ;
-}
-
-list.prototype.pop_back = function() {
-    if (this.length !== 0) {
-        let val = this.last.value;
-        this.last = this.last.prev;
-        if (this.length !== 1) {
-            this.last.next = null;
-        }
-        else {
+    push_back(data) {
+        if (this.length === 0) {
+            this.last = new node(data, null, null);
             this.first = this.last;
         }
-        this.length--;
-		return val;
-    }
-    return ;
-}
-
-list.prototype.print = function() {
-    if (this.length > 0) {
-	    let n = this.first;
-	    while (n !== null) {
-            console.log(n.value);
-	    	n = n.next;
+        else {
+            this.last.next = new node(data, this.last, null);
+            this.last = this.last.next;
         }
+        this.length++;
     }
-    console.log("");
-}
 
-list.prototype.clear = function() {
-    this.first = null;
-    this.last = null;
-    this.length = 0;
-}
-
-list.prototype.size = function() {
-    return this.length;
-}
-
-function swap(list, i, j) {
-    console.log('swap');
-    console.log('j =   ' + j.value);
-    console.log('i =   ' + i.value);
-
-    if (i !== j) {
-        if (i.next === j) {
-
+    push_front(data) {
+        this.first.prev = new node(data, null, this.first);
+        this.first = this.first.prev;
+        if (this.length === 0) {
+            this.last = this.first;
         }
-        let tmp = new node(j.value, j.prev, j.next);
-        if (i.prev === null && j.next === null) {
-            //i.prev.next = j;
-            i.next.prev = j;
-            //j.next.prev = i;
-            j.prev.next = i;
-            j.next = i.next;
-            j.prev = i.prev;
-            i.next = tmp.next;
-            i.prev = tmp.prev;
-            list.first = j;
-            list.last = i;
+        this.length++;
+    }
+
+    insert(val, ind) {
+        if (ind === this.length) {
+            this.push_back(val);
         }
         else {
-            if (j.prev === null) {
-                    i.prev.next = j;
-                    i.next.prev = j;
-                    j.next.prev = i;
-                    //j.prev.next = i;
-                    j.next = i.next;
-                    j.prev = i.prev;
-                    i.next = tmp.next;
-                    i.prev = tmp.prev;
-                    list.first = i;                
-            }
-            if (j.next === null) {
-                    i.prev.next = j;
-                    i.next.prev = j;
-                    //j.next.prev = i;
-                    j.prev.next = i;
-                    j.next = i.next;
-                    j.prev = i.prev;
-                    i.next = tmp.next;
-                    i.prev = tmp.prev;
-                    list.last = i;  
-            }
-            else {
-                if (i.prev === null) {
-                    //i.prev.next = j;
-                    i.next.prev = j;
-                    j.next.prev = i;
-                    j.prev.next = i;
-                    j.next = i.next;
-                    j.prev = i.prev;
-                    i.next = tmp.next;
-                    i.prev = tmp.prev;
-                    list.first = j; 
+            let n = this.find(ind);
+            if (n !== undefined) {
+                if (ind === 0) {
+                    this.push_front(val);
                 }
                 else {
-                    i.prev.next = j;
-                    i.next.prev = j;
-                    j.next.prev = i;
-                    j.prev.next = i;
-                    j.next = i.next;
-                    j.prev = i.prev;
-                    i.next = tmp.next;
-                    i.prev = tmp.prev;
+                    let np = n.prev;
+                    n.prev = new node(val, np, n);
+                    np.next = n.prev;
+    
+                }
+                this.length++;
+            }
+        }
+    }
+
+    isEmpty() {
+        return this.length === 0;
+    }
+
+    find(ind) {
+        if (isNaN(Number(ind)) || ind >= this.length || ind < 0) {
+            return;
+        }
+        let tmp = this.first;
+        for (let i = 0; i < ind; i++) {
+            tmp = tmp.next;
+        }
+        return tmp;
+    }
+
+    remove(ind) {
+        let n = this.find(ind);
+        if (n !== undefined && this.length > 0) {
+            if (ind !== 0 && ind !== this.length - 1) {
+                n.prev.next = n.next;
+                n.next.prev = n.prev;
+            }
+            if (ind === 0) {
+                this.pop_front();
+            }
+            if (ind === this.length -1) {
+                this.pop_back();
+            }
+        }
+    }
+
+    pop_front() {
+        if (this.length !== 0) {
+            let val = this.first.value;
+            this.first = this.first.next;
+            if (this.length !== 1) {
+                this.first.prev = null;
+            }
+            else {
+                this.last = this.first;
+            }
+            this.length--;
+            return val;
+        }
+        return ;
+    }
+
+    pop_back() {
+        if (this.length !== 0) {
+            let val = this.last.value;
+            this.last = this.last.prev;
+            if (this.length !== 1) {
+                this.last.next = null;
+            }
+            else {
+                this.first = this.last;
+            }
+            this.length--;
+            return val;
+        }
+        return ;
+    }
+
+    print() {
+        if (this.length > 0) {
+            let n = this.first;
+            while (n !== null) {
+                console.log(n.value);
+                n = n.next;
+            }
+        }
+        console.log("");
+    }
+
+    rprint() {
+        if (this.length > 0) {
+            let n = this.last;
+            while (n !== null) {
+                console.log(n.value);
+                n = n.prev;
+            }
+        }
+        console.log("");
+    }
+
+    clear() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    size() {
+        return this.length;
+    }
+
+    insertNode(node, pr) {
+        if (node){
+            if(pr === null) {
+                this.first.prev = node;
+                node.next = this.first;
+                node.prev = null;
+                this.first = node;
+            }
+            else {
+                if (this.last.prev == pr.prev) {
+                    this.last.next = node;
+                    node.next = null;
+                    node.prev = this.last;
+                    this.last = node;
+                }
+                else {
+                    pr.next.prev = node;
+                    node.next = pr.next;
+                    pr.next = node;
+                    node.prev = pr;
+                    this.length++;
+                }
+            }
+        }
+    }
+
+    removeNode (node) {
+        if (node && this.length > 0) {
+            if (node !== this.first && node !== this.last) {
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+                this.length--;
+            }
+            else {
+                if(node == this.first) {
+                    this.pop_front();
+                }
+                else {
+                    this.pop_back();
                 }
             }
         }
     }
 }
 
-function partition (list, f, l) {
-    let piv = l;
-    let i = f.prev;
-    for (var j = f; j.next.next !== null; /*j = j.next*/) {
-        let J = new node(j.value, j.prev, j.next);
-        console.log('for piv = ' + piv.value)
-        console.log('for j = ' + j.value)
-        if (j.value >= piv.value) {
-            console.log('if')
-            if(i === null) {
-                i = f;
+function partSort (lis, begin, end) {
+    let pivot = begin;
+    let curr = begin.next;
+    while (curr && curr != end.next) {
+        let tmp1 = Object.assign({}, curr);
+        if (curr.value < pivot.value) {
+            let tmp2 = Object.assign({}, curr);
+            if(curr == end) {
+                end = end.prev;
             }
-            else {
-                i = i.next;
-            }
-            console.log('for i = ' + i.value)
-            swap(list, i, j);
+            lis.removeNode(curr);
+            lis.insertNode(tmp2, begin.prev);
+            begin = begin.prev;
         }
-        j = J.next;
+        curr = tmp1.next;
     }
-        if(i === null) {
-            i = f;
-        }
-        else {
-        i = i.next;}
-    let ind = new node(i.val, i.prev, i.next);
-    swap(list, i, piv);
-    return ind;
+    return { pivot: pivot, begin: begin, end: end };
 }
 
-function quickSortForList(list, f, l) {
-    console.log('q')
-    if (f !== l) {
-        let piv = partition(list, f, l);
-        if (piv.prev !== null && piv.prev !== f) {
-            quickSortForList(list, f, piv.prev);
-        }
-        if (piv.next !== null && piv.next !== l) {
-            quickSortForList(list, piv.next, l);
-        }
+function QSort (lis, begin, end) {
+    if (!begin || !end || begin == end) {
+        return lis;
+    }
+    let pivObj = partSort (lis, begin, end);
+    let piv = pivObj.pivot;
+    begin = pivObj.begin;
+    end = pivObj.end;
+    if (!piv) {
+        return lis;
+    }
+    if (begin != piv.prev && begin != piv) {
+        QSort(lis, begin, piv.prev);
+    }
+    if (piv.next != end && piv != end) {
+        QSort(lis, piv.next, end);
     }
 }
 
 let count = NaN;
-let l = new list();
+let lis = new list();
+
 while (isNaN(Number(count))) {
     count = readline.question("Enter list element count\n");    
 }
 
 for (let i = 0; i < count; i++) {
-    l.push_back(Math.floor(Math.random()* 100));
+    lis.push_back(Math.floor(Math.random()* 100));
 }
 
-l.print();
-
-quickSortForList(l, l.first, l.last);
-
-//l.print();
-
-
+lis.print();
+QSort (lis, lis.first, lis.last);
+console.log('xxxxxxxxxxxxxxxx');
+lis.print();
