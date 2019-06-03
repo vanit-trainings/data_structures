d74dbc63a2517d67c8e01eadbb848355651e0d44
Hrach007 <hrach.asatryan.1998@mail.ru>
2019-06-03 16:03:20 +0400

    Added random fail for insert

--- Hrach/database/generator.dart
+++ Hrach/database/generator.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
+// import 'dart:svg';
+
 const AsciiCodec asc = AsciiCodec();
 Random r = Random();
 Set<String> email = Set();
@@ -10,6 +12,9 @@ Set<String> webPage = Set();
 
 int randomNumberInRange(int min, int max) {
   return (r.nextInt(max - min) + min);
+}
+dynamic randomNumberInRangeFloat(int min, int max) {
+  return (r.nextInt(max - min) + min).toDouble();
 }
 
 String nameGenerator() {
@@ -126,11 +131,62 @@ String webPageGenerator() {
 
 main(List<String> args) {
   const int size = 1000;
-  File f = File('artadrox.txt');
+  File f = File('pahest.txt');
   String s;
+  // artadroxner
   for (int i = 0; i < size; ++i) {
-    s = "insert into producers(name,address,phone,email,web_page) values ('${nameGenerator()}','${addressGenerator()}','${phoneGenerator()}','${emailGenerator()}','${webPageGenerator()}')\n";
+    s = "INSERT INTO public.producer(name,address,phone,email,web_page) VALUES ('${nameGenerator()}','${addressGenerator()}','${phoneGenerator()}','${emailGenerator()}','${webPageGenerator()}');\n";
     f.writeAsStringSync(s, mode: FileMode.append);
   }
   //////////////////
+  // patviratuner
+  for (int i = 0; i < size; ++i) {
+    s = "INSERT INTO public.costumer(name,address,phone,email) VALUES ('${nameGenerator()}','${addressGenerator()}','${phoneGenerator()}','${emailGenerator()}');\n";
+    f.writeAsStringSync(s, mode: FileMode.append);
+  }
+  // Hastiq
+  for (int i = 0; i < size; ++i) {
+    s = "INSERT INTO public.occupation(name) VALUES ('${nameGenerator()}');\n";
+    f.writeAsStringSync(s, mode: FileMode.append);
+  }
+  // Points
+  for (int i = 0; i < size; ++i) {
+    s = "INSERT INTO public.points(name) VALUES ('${nameGenerator()}');\n";
+    f.writeAsStringSync(s, mode: FileMode.append);
+  }
+  // Pahest
+  for (int i = 0; i < size; ++i) {
+    s = "INSERT INTO public.storage(name,space_m3,address,start_time,finish_time) VALUES ('${nameGenerator()}', ${randomNumberInRangeFloat(100, 10000)},'${addressGenerator()}','${timeGenerator()}','${timeGenerator()}');\n";
+    f.writeAsStringSync(s, mode: FileMode.append);
+  }
+  // ashxatox
+  for (int i = 0; i < size; ++i) {
+    s = "INSERT INTO public.workers(name,surname,birthdate,address,phone,email,salary,occupation_id) VALUES ('${nameGenerator()}','${surNameGenerator()}', '${birthDateGenerator()}', '${addressGenerator()}', '${phoneGenerator()}', '${emailGenerator()}', ${randomNumberInRangeFloat(100000, 300000)}, ${randomNumberInRange(1, 1000)});\n";
+    f.writeAsStringSync(s, mode: FileMode.append);
+  }
+  // apranqner
+  for (int i = 0; i < size; ++i) {
+    s = "INSERT INTO public.products(name,point_id,expiration_date,point_weight,point_volume_m3,production_date,producer_id,product_average_price) VALUES ('${nameGenerator()}',${randomNumberInRange(1, 1000)}, '${birthDateGenerator()}', ${randomNumberInRangeFloat(1, 1000)}, ${randomNumberInRangeFloat(1, 1000)}, '${birthDateGenerator()}', ${randomNumberInRange(1, 1000)}, ${randomNumberInRangeFloat(10, 2000)});\n";
+    f.writeAsStringSync(s, mode: FileMode.append);
+  }
+  // mutq
+  for (int i = 0; i < size; ++i) {
+    s = "INSERT INTO public.recieve(date_day,product_id,producer_id,quantity,point_id,worker_id,recieve_price,storage_id) VALUES ('${birthDateGenerator()}', ${randomNumberInRange(1, 1000)}, ${randomNumberInRange(1, 1000)}, ${randomNumberInRange(1, 1000)}, ${randomNumberInRange(1, 1000)}, ${randomNumberInRange(1, 1000)}, ${randomNumberInRangeFloat(10, 2000)}, ${randomNumberInRange(1, 1000)});\n";
+    f.writeAsStringSync(s, mode: FileMode.append);
+  }
+  // elq
+  for (int i = 0; i < size; ++i) {
+    s = "INSERT INTO public.deliveris(date_day,product_id,costumer_id,quantity,point_id,worker_id,delivery_price,storage_id) VALUES ('${birthDateGenerator()}', ${randomNumberInRange(1, 1000)}, ${randomNumberInRange(1, 1000)}, ${randomNumberInRange(1, 1000)}, ${randomNumberInRange(1, 1000)}, ${randomNumberInRange(1, 1000)}, ${randomNumberInRangeFloat(10, 2000)}, ${randomNumberInRange(1, 1000)});\n";
+    f.writeAsStringSync(s, mode: FileMode.append);
+  }
+  // workers -> storage
+  for (int i = 0; i < size; ++i) {
+    s = "INSERT INTO public.worker_storage(worker_id,storage_id) VALUES (${randomNumberInRange(1, 1000)}, ${randomNumberInRange(1, 1000)});\n";
+    f.writeAsStringSync(s, mode: FileMode.append);
+  }
+  // producer -> product
+  for (int i = 0; i < size; ++i) {
+    s = "INSERT INTO public.producer_product(producer_id,product_id) VALUES (${randomNumberInRange(1,1000)}, ${randomNumberInRange(1, 1000)});\n";
+    f.writeAsStringSync(s, mode: FileMode.append);
+  }
 }
